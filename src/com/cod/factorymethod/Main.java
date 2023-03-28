092eb13ad3007e86b2a32551e8b04423e5204461
Adrian <avideirarial@danielcastelao.org>
2023-03-28 13:16:05 +0200

    Nueva rama: se añade transporte Barco

--- src/com/cod/factorymethod/Main.java
+++ src/com/cod/factorymethod/Main.java
@@ -17,6 +17,17 @@ public class Main {
 
         Transportes valroNulo = FabricaTransporte.crearTransporte("Cosa");
 
+        System.out.println("**************************************************************");
+
+        /**
+         * Instanciamos nuevo transporte @Barco.
+         * En la nueva rama llamada barco.
+         */
+
+        Transportes barco1 = FabricaTransporte.crearTransporte("Barco");
+        barco1.costeTotal(2000);
+        barco1.embalaje(2500,1500,800,300);
+
 
 
     }
--- src/com/cod/factorymethod/TransportesFactory.java
+++ src/com/cod/factorymethod/TransportesFactory.java
@@ -8,7 +8,7 @@ public class TransportesFactory {
      * @return nuevo Transporte (Camion, bicicleta o desconocido)
      */
 
-    public Transportes crearTransporte (String TipoTransporte){
+        public Transportes crearTransporte (String TipoTransporte){ //METODO publico que devuelve objetos según patron recibido
         if (TipoTransporte == null || TipoTransporte.isEmpty())
             return null;
         switch (TipoTransporte){
@@ -16,9 +16,13 @@ public class TransportesFactory {
                 return new Camion();
             case "Bicicleta":
                 return new Bicicleta();
+            case "Barco"://Añadimos a la fabrica de transportes la opcion barco para poder instanciarla.
+                return  new Barco();
             default:
                 throw new IllegalArgumentException("Transporte desconocido "+ TipoTransporte);
         }
+
+
 
     }
 
