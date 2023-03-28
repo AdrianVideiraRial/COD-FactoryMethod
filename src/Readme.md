1d2358d3be8b2e7f20be1e9143339d7e1123c78d
Adrian <avideirarial@danielcastelao.org>
2023-03-28 13:17:16 +0200

    Nueva rama: se añade transporte Barco

---
+++ src/Readme.md
@@ -0,0 +1,9 @@
+# Creacion nueva rama "barco."
+
+Evolucion proyecto FactoryMethod.
+
+Con la nueva rama `barco` del proyecto, se crea un nuevo vehículo de transporte.
+Para ello hemos creado la nueva clase `Barco` y en ella se han 
+implementado la `Interface Transportes` con todos sus metodos.
+
+
---
+++ src/com/cod/factorymethod/Barco.java
@@ -0,0 +1,58 @@
+package com.cod.factorymethod;
+
+public class Barco implements Transportes{
+
+    /**
+     * Variables
+     */
+    float coste;
+    int tipoEmbalaje;
+
+    /**
+     * Metodos.
+     * @author Adrian Videira
+     * @version 2.0
+     * @param cp
+     * @return coste
+     */
+
+    @Override
+    public float costeTotal(int cp) {
+        if (cp<10){
+            coste = 0.5f;
+            System.out.println("El coste del transporte para este cp es:" + coste);
+        }if (cp>10 && cp<210){
+            coste = 17.85f;
+            System.out.println("El coste del transporte para este cp es: " + coste);
+        }if (cp>=210){
+            coste = 23.35f;
+            System.out.println("El coste del transporte para este cp es:" + coste);
+        }
+        return coste;
+    }
+
+    /**
+     * @author Adrian Videira
+     * @version 2.0
+     * @param x (ancho)
+     * @param y (alto)
+     * @param z (largo)
+     * @param peso
+     * @return tipoEmbalaje
+     */
+
+    @Override
+    public int embalaje(float x, float y, float z, float peso) {
+        if ((x>=2000 || y>=2000 || z>=2000) && (peso>=500)){
+            tipoEmbalaje = 0;
+            System.out.println("El tipo de embalaje es un pallet");
+        }if ((x<2000 || y<2000 || z<2000) && (peso<500)){
+            tipoEmbalaje = 1;
+            System.out.println("El tipo de embalaje es una caja de cartón");
+        }if ((x<500 || y<500 || z<500) && (peso<=100)) {
+            tipoEmbalaje = 2;
+            System.out.println("El tipo de embalaje es una caja de madera");
+        }
+        return tipoEmbalaje;
+    }
+}
